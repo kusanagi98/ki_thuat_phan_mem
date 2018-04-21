3f99dfebda1adab54d5f0d917551ef81956fd64a
kusanagi98 <36164826+kusanagi98@users.noreply.github.com>
2018-04-21 20:36:40 +0700

    Add files via upload

---
+++ MyApplication2/app/src/main/java/com/example/admin/myapplication/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.admin.myapplication.app;
+
+/**
+ * Created by admin on 4/8/2018.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.1.105/hedspi_login/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.1.105/hedspi_login/android_login_api/register.php";
+}
